d3697df6da66537f804e16507e5da98a121969e9
Chad Bruhaug <churfzilla@gmail.com>
2016-09-13 11:20:32 -0700

    Gives user error reports in input is incorrect

--- download_avatars.js
+++ download_avatars.js
@@ -2,10 +2,33 @@
 const contributors = require('./contributors.js');
 //uses the npm dotenv - https://www.npmjs.com/package/dotenv
 require('dotenv').config();
+//Checks if the user correctly set the user and token
+if (process.env.USER === undefined || process.env.GIT_API_TOKEN === undefined) {
+  console.log('GitHub API user and token not defined. (AGENT, GIT_API_TOKEN). Enter correct User and Token in .env file');
+  console.log('Exiting.');
+  process.exit();
+}
+
 // Takes the argv[2] as the user input and the argv[3] as the repo input
+
 function getUserInput() {
   const userInput = process.argv;
-  return { user: userInput[2], repo: userInput[3]};
+  var err = '';
+// handeling errors with switch and case
+//docs used - https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Statements/switch
+  switch (userInput.length) {
+    case 4: return { user: userInput[2], repo: userInput[3]};
+    break;
+    case 3: err = 'No repository info provided.';
+    break;
+    case 2: err = 'No username or repo provided.';
+    break;
+    default: err = 'Unknown user input.';
+  }
+  console.log(err, 'Please provide a username and repository:');
+  console.log('node download_avatars.js <userName> <repositoryName>');
+  return;
+
 }
 //simplfiying the function
 var userInput = getUserInput();
@@ -17,3 +40,7 @@ contributors.get(userInput.user, userInput.repo, contributors.downloadAvatars);
 //check git users in repo - loop through all contributors
   //find avatar_urls
     //determine files and save correct types to /Avatars folder
+
+
+
+// Error handling docs - http://benno.id.au/blog/2011/08/08/nodejs-exceptions
